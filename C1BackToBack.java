a8dd788bf3247ecaca8a030cb0b2788a0e672bd4
TylerChu1234 <tylerchu.tyc@gmail.com>
2024-04-03 07:51:56 -0400

    awdawdasdasd

--- C1BackToBack.java
+++ C1BackToBack.java
@@ -6,6 +6,20 @@ class C1BackToBack extends ConsoleProgram {
   */
   
   public void run() {
+    String strWord;
+    String strLastcha;
+    String strNewword;
+
+    strWord = readLine("Enter a word: ");
+
+
+    strLastcha = strWord.substring(strWord.length() -1);
+
+    strNewword = strLastcha+strWord+strLastcha;
+
+    System.out.println(strNewword);
+
+
     
   }
 }
